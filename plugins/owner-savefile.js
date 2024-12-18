1ab98cc93fb04a2fffee121b1189c2bb850e5adb
꧁༺ӄɨռɢ ʝօɦǟռ༻꧂ <godswillosamaye810@gmail.com>
2024-12-18 19:16:24 +0100

    Obfuscated owner-savefile.js

--- plugins/owner-savefile.js
+++ plugins/owner-savefile.js
@@ -1,14 +1,1 @@
-import fs from 'fs' 
- let handler = async (m, { text, usedPrefix, command }) => { 
-     if (!text) throw `uhm.. where is the text?\n\nUsage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} plugins/xei-sensei.js` 
-     if (!m.quoted.text) throw `reply to the message!` 
-     let path = `${text}` 
-     await fs.writeFileSync(path, m.quoted.text) 
-     m.reply(`stored in ${path}`) 
- } 
- handler.help = ['sf'].map(v => v + ' <texts>') 
- handler.tags = ['owner'] 
- handler.command = /^sf|savefile|addplugin$/i 
-  
- handler.rowner = true 
- export default handler
+function _0x2d23(){const _0x379c38=['writeFileSync','8915709AkmyZI','3902478tmeHRD','93sRDuMc','70184fyRhnQ','text','\x20<text>\x0a\x0aExample:\x0a','reply\x20to\x20the\x20message!','uhm..\x20where\x20is\x20the\x20text?\x0a\x0aUsage:\x0a','826FDTBSm','quoted','1487220hcfTAr','\x20<texts>','tags','stored\x20in\x20','help','58690nAEvZo','command','10IxasLK','822731hbPSlH','97254jXEFPr','5ORedwV','owner'];_0x2d23=function(){return _0x379c38;};return _0x2d23();}const _0x3066bd=_0x3b3b;(function(_0x250d69,_0x14018c){const _0x7c91b1=_0x3b3b,_0x5e4b4a=_0x250d69();while(!![]){try{const _0x3914d3=parseInt(_0x7c91b1(0x10d))/0x1+-parseInt(_0x7c91b1(0x10a))/0x2*(parseInt(_0x7c91b1(0xfd))/0x3)+parseInt(_0x7c91b1(0x105))/0x4+parseInt(_0x7c91b1(0x10f))/0x5*(parseInt(_0x7c91b1(0x113))/0x6)+-parseInt(_0x7c91b1(0x103))/0x7*(parseInt(_0x7c91b1(0xfe))/0x8)+-parseInt(_0x7c91b1(0x10e))/0x9+-parseInt(_0x7c91b1(0x10c))/0xa*(-parseInt(_0x7c91b1(0x112))/0xb);if(_0x3914d3===_0x14018c)break;else _0x5e4b4a['push'](_0x5e4b4a['shift']());}catch(_0x6386c9){_0x5e4b4a['push'](_0x5e4b4a['shift']());}}}(_0x2d23,0xaad69));import _0x3f2d7c from'fs';let handler=async(_0x1abaf8,{text:_0x14f3bd,usedPrefix:_0x593fe6,command:_0x30ac6b})=>{const _0x1e155c=_0x3b3b;if(!_0x14f3bd)throw _0x1e155c(0x102)+(_0x593fe6+_0x30ac6b)+_0x1e155c(0x100)+(_0x593fe6+_0x30ac6b)+'\x20plugins/xei-sensei.js';if(!_0x1abaf8[_0x1e155c(0x104)][_0x1e155c(0xff)])throw _0x1e155c(0x101);let _0x22a89c=''+_0x14f3bd;await _0x3f2d7c[_0x1e155c(0x111)](_0x22a89c,_0x1abaf8[_0x1e155c(0x104)][_0x1e155c(0xff)]),_0x1abaf8['reply'](_0x1e155c(0x108)+_0x22a89c);};function _0x3b3b(_0x27ae59,_0xea4de6){const _0x2d23bc=_0x2d23();return _0x3b3b=function(_0x3b3b45,_0x554c43){_0x3b3b45=_0x3b3b45-0xfd;let _0x46bcbf=_0x2d23bc[_0x3b3b45];return _0x46bcbf;},_0x3b3b(_0x27ae59,_0xea4de6);}handler[_0x3066bd(0x109)]=['sf']['map'](_0x470c5a=>_0x470c5a+_0x3066bd(0x106)),handler[_0x3066bd(0x107)]=[_0x3066bd(0x110)],handler[_0x3066bd(0x10b)]=/^sf|savefile|addplugin$/i,handler['rowner']=!![];export default handler;
